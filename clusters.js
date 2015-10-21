276013c901498d8d357430fd3d09752c901ef9d3
Marc-Andre Beaudry <marcandre.beaudry2@gmail.com>
2015-10-20 22:27:25 -0400

    WIP

--- clusters.js
+++ clusters.js
@@ -26,12 +26,14 @@ App.controller('clustersController', function($scope, $http, $location, $routePa
 	$scope.loadData = function() {	
 		$scope.isLoadingData = true;
 		clusterApi.getClusters().then(
+			//success callback
 			function(data, status) {
 				receivedClusters = data;		
 				$scope.filter();
 				$scope.isLoadingData = false;
 				$scope.$apply();	
 			}, 
+			//failure callback
 			function(data, status) {
 				$scope.isLoadingData = false;
 				$scope.$apply();	
@@ -40,13 +42,20 @@ App.controller('clustersController', function($scope, $http, $location, $routePa
 	}
 		
 	$scope.startInstance = function(instance) {
+		// TODO Replace this because browser dialog is not shiny.
+		if(!confirm("Do you want to start that instance?")) {
+			return;
+		}
+	
 		instance.actionInProgress = true;
 		clusterApi.startInstance(instance).then(
+			//success callback
 			function(data, status) {			
 				instance.actionInProgress = false;
 				instance.status = "NotHealthy";
 				$scope.$apply();	
 			}, 
+			//failure callback
 			function(data, status) {
 				instance.actionInProgress = false;				
 				$scope.$apply();	
@@ -55,16 +64,65 @@ App.controller('clustersController', function($scope, $http, $location, $routePa
 	}
 	
 	$scope.stopInstance = function(instance) {
+		// TODO Replace this because browser dialog is not shiny.
+		if(!confirm("Do you want to stop that instance?")) {
+			return;
+		}
+	
 		instance.actionInProgress = true;
 		clusterApi.stopInstance(instance).then(
+			//success callback
 			function(data, status) {			
 				instance.actionInProgress = false;
 				instance.status = "Offline";
 				$scope.$apply();	
 			}, 
+			//failure callback
 			function(data, status) {
 				instance.actionInProgress = false;
 				$scope.$apply();	
+			}
+		);
+	}
+	
+	$scope.deleteCluster = function(cluster) {
+		// TODO Replace this because browser dialog is not shiny.
+		if(!confirm("Do you want to delete that cluster?")) {
+			return;
+		}
+		cluster.actionInProgress = true;
+		clusterApi.deleteCluster(cluster).then(
+			//success callback
+			function(data, status) {
+				// Remove cluster from list
+				// Cluster is equal if proid.ownerId.name.id match.
+				for(var i = receivedClusters.length - 1; i >= 0; i--) {
+					if(	receivedClusters[i].proid == cluster.proid && 
+						receivedClusters[i].ownerId == cluster.ownerId && 
+						receivedClusters[i].name == cluster.name && 
+						receivedClusters[i].id == cluster.id) {
+						receivedClusters.splice(i, 1);
+					}
+				}
+				$scope.filter();
+				cluster.actionInProgress = false;
+				$scope.$apply();	
+			}, 
+			//failure callback
+			function(data, status) {
+				cluster.actionInProgress = false;
+				$scope.$apply();	
+			}
+		);
+	}
+	
+	$scope.getCreateLBScript = function(cluster) {
+		clusterApi.getCreateLBScript(cluster).then(
+			function(data, status) {
+				confirm("asdasdasd \n asdasdasd \n asdasdasd \n asdasdasd \n asdasdasd \n");
+			},
+			function(data, status) {
+			
 			}
 		);
 	}
